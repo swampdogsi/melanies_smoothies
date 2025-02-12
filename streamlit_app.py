7ca958a78579e02aae1105eda79e5e2b43bf082e
swampdogsi <simon@insightfulconsulting.co.nz>
2025-02-12 15:41:33 +1300

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,14 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
 st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
 st.write(
     """Orders that need to be filled
     """)
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+
 
 from snowflake.snowpark.functions import col, when_matched
 
