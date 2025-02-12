e95d529e5518208e370b53473ad282c9a7f254cf
swampdogsi <simon@insightfulconsulting.co.nz>
2025-02-12 18:15:57 +1300

    Update streamlit_app.py
    Correct code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,19 @@
 # Import python packages
 import streamlit as st
+from snowflake.snowpark.functions import col
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom Smoothie!
     """)
-cnx = st.connection("snowflake")
-session = cnx.session()
-
-
-from snowflake.snowpark.functions import col, when_matched
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
